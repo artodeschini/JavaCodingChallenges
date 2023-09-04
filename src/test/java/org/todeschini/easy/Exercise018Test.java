b1af5778cd39c444706dee42eb2e001cfe6a736a
Artur Todeschni Crestani <artodeschini@gmail.com>
2023-09-04 00:57:36 -0300

    add unit test

--- src/test/java/org/todeschini/easy/Exercise018Test.java
+++ src/test/java/org/todeschini/easy/Exercise018Test.java
@@ -15,9 +15,9 @@ class Exercise018Test {
 
     @Test
     public void testReverseArrayLengthFive() {
-        assertArrayEquals(new int[]{5,4,3,2,1}, Exercise018.reverseArray(new int[]{1,2,3,4,5}));
-        assertArrayEquals(new int[]{5,4,3,2,1}, Exercise018.reverseArrayWithStream(new int[]{1,2,3,4,5}));
-        assertArrayEquals(new int[]{5,4,3,2,1}, Exercise018.reverseArrayWithCollection(new int[]{1,2,3,4,5}));
+        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, Exercise018.reverseArray(new int[]{1, 2, 3, 4, 5}));
+        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, Exercise018.reverseArrayWithStream(new int[]{1, 2, 3, 4, 5}));
+        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, Exercise018.reverseArrayWithCollection(new int[]{1, 2, 3, 4, 5}));
     }
 
     @Test
